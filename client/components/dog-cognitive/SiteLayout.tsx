4d4e1dd1ca6355df418c6489182a232ff3f9d8ab
valentinick521-star <valentinick521@gmail.com>
2026-08-13 17:25:40 -0500

    Load compact PawPrint summary

--- client/components/dog-cognitive/SiteLayout.tsx
+++ client/components/dog-cognitive/SiteLayout.tsx
@@ -7,6 +7,7 @@ import "../../../dog-cognitive-page/full-comparison-buying-copy.js";
 import "../../../dog-cognitive-page/full-comparison-corrections.js";
 import "../../../dog-cognitive-page/final-cta-cleanup.js";
 import "../../../dog-cognitive-page/accuracy-cleanup.js";
+import "../../../dog-cognitive-page/compact-why-pawprint.js";
 import { ReactNode, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import SiteHeader from "./SiteHeader";
