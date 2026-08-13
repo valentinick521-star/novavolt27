6adc38e03fa65c05433ba9c735e82598f1a5b3b1
valentinick521-star <valentinick521@gmail.com>
2026-08-13 14:42:59 -0500

    Load lower comparison cleanup

--- client/components/dog-cognitive/PawprintCta.tsx
+++ client/components/dog-cognitive/PawprintCta.tsx
@@ -1,4 +1,5 @@
 import "../../../dog-cognitive-page/full-comparison-rewrite.js";
+import "../../../dog-cognitive-page/lower-page-cleanup.js";
 import { ReactNode } from "react";
 import {
   buildPawprintUrl,
