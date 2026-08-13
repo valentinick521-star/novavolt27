11a18adaacf94500ce066bc6b5cc2fc63ed46a9c
valentinick521-star <valentinick521@gmail.com>
2026-08-13 15:26:53 -0500

    Load cognitive page accuracy cleanup

--- client/components/dog-cognitive/SiteLayout.tsx
+++ client/components/dog-cognitive/SiteLayout.tsx
@@ -5,6 +5,7 @@ import "@/styles/dog-cognitive-4.css";
 import "@/styles/dog-cognitive-5.css";
 import "../../../dog-cognitive-page/full-comparison-buying-copy.js";
 import "../../../dog-cognitive-page/final-cta-cleanup.js";
+import "../../../dog-cognitive-page/accuracy-cleanup.js";
 import { ReactNode, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import SiteHeader from "./SiteHeader";
