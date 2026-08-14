6105a07f0a7cd336d239582ca1ffac5c2411f770
valentinick521-star <valentinick521@gmail.com>
2026-08-13 22:25:09 -0500

    Add intrinsic logo dimensions

--- client/components/dog-cognitive/SiteHeader.tsx
+++ client/components/dog-cognitive/SiteHeader.tsx
@@ -27,6 +27,8 @@ export default function SiteHeader() {
           <img
             src="https://cdn.builder.io/api/v1/image/assets%2Ff12907698ec44301a20b66b5fc338f8f%2F6e07a1b8bd4c41b7b6b7bc44ee60c192?format=webp&width=300&quality=70"
             alt="National Consumer Review"
+            width={300}
+            height={64}
             decoding="async"
           />
         </Link>
